bebb50890b398f280d157e9e151c90d93c76fea3
Jesus <deleterr1996@ukr.net>
2017-09-21 10:54:43 +0300

    Add repository interfaces

--- src/main/java/repository/EmployeeRepository.java
+++ src/main/java/repository/EmployeeRepository.java
@@ -2,6 +2,7 @@ package repository;
 
 import entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -10,4 +11,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query(value = "SELECT * FROM EMPLOYEE WHERE FIRST_NAME=?1 AND LAST_NAME=?2", nativeQuery = true)
+    Employee findByNameAndLastName(String firstName, String lastName);
+
+    Employee findByLastName(String lastName);
 }
